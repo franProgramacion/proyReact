550d4770529e05278e7095b6fd0b9d327f1bd4e8
Pablo Dominguez <pablo.dm666@gmail.com>
2022-07-22 16:12:03 -0300

    segundo desafio

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+const Navbar =() => {
+    return (
+        <nav>
+            <div className="cabecera">
+                <h1> Eco Natural: Tienda oficial de Natura en Santiago del Estero</h1>
+                <img src='imagenes/Logo.png' alt='logo' className="imag" />
+                <div className="Navbar">
+                <button className="botones">Perfumes</button>
+                <button className="botones">Desodorantes</button>
+                <button className="botones">Cuidados diarios</button>
+                <button className="botones">Jabones</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
